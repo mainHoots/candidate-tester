40664fe82e751fc23ff6063053b7f735cfce5215
Brian Hoots <thehoots26@gmail.com>
2021-01-31 23:10:50 +0000

    Fulfilling coding requirements for Part 3 of Assignment #1

--- index.js
+++ index.js
@@ -8,25 +8,44 @@
 
 const input = require('readline-sync');
 
-let name = input.question("Please enter your name: ");
+// let name = input.question("Please enter your name: ");
 
-let questionList = [
-["True or false: 5000 meters = 5 kilometers ", "True"],
-["(5 + 3)/2 * 10 = ? ", 40],
-["Given the array, [8, Orbit, Trajectory, 45], what entry is at index 2", "Trajectory"],
-["Who was the first American woman in space?", "Sally Ride"],
-["What is the new minimum crew size for the International Space Station (ISS)? ", "3"]
+let quizQuestionsAnswers = [
+  
+  ["True or false: 5000 meters = 5 kilometers ", "True"],
+  ["(5 + 3)/2 * 10 = ? ", 40],
+  ["Given the array, [8, Orbit, Trajectory, 45], what entry is at index 2", "Trajectory"],
+  ["Who was the first American woman in space?", "Sally Ride"],
+  ["What is the new minimum crew size for the International Space Station (ISS)? ", "3"],
+
 ];
 
+let quizQuestions = [];
 
-// console.log(questionList[0][1])
+let quizAnswers = [];
 
-let oneQuestion = input.question(questionList[0][0]);
+// console.log(quizQuestionsAnswers.length);
 
-let lowerCaseAnswer = questionList[0][1].toLowerCase();
+// console.table(quizQuestionsAnswers);
 
-if (oneQuestion.toLowerCase() === lowerCaseAnswer) {
-  console.log(`"Yes, ${name}, that is correct!"`);
-} else {
-  console.log(`"Oh dear, ${name}, that is incorrect."`)
-}
+// for (i = 0; i < quizQuestionsAnswers.length; i++) {
+//   console.log("No mountain high enough!");
+
+// };
+
+
+
+// console.log(quizQuestionsAnswers[0][1])
+
+let oneQuestion = input.question(quizQuestionsAnswers[0][3]);
+
+console.log(oneQuestion);
+
+// let lowerCaseAnswer = quizQuestionsAnswers[0][1].toLowerCase();
+
+// if (oneQuestion.toLowerCase() === lowerCaseAnswer) {
+//   console.log(`"Yes, ${name}, that is correct!"`);
+// } else {
+//   console.log(`"Oh dear, ${name}, that is incorrect."`)
+// }
+
